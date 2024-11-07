68a4d49c72e828392787a31eb9bdc5651e84e00c
vinfreecs <89305950+vinfreecs@users.noreply.github.com>
2024-11-07 19:47:43 +0100

    Repo is pip installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='0.0.1',
+    description="dsss_homework_2",
+    url= 'https://github.com/vinfreecs/dsss_homework_2',
+    author='Vinay Kumar',
+    author_email='vinayrbs001@gmail.com',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version<"3.10"',
+    ],
+)
